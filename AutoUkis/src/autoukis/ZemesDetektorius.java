93d893d86714870c09d3d19155f55ce3482c7cf0
Matas Savickis <savickismatas@gmail.com>
2017-10-08 19:56:11 +0300

    rnd (#77)
    rnd

--- AutoUkis/src/autoukis/ZemesDetektorius.java
+++ AutoUkis/src/autoukis/ZemesDetektorius.java
@@ -1,7 +1,52 @@
 package autoukis;
 
 import java.io.Serializable;
+import java.util.Random;
 
 public class ZemesDetektorius implements Serializable{
 
+    Random rnd = new Random();
+    
+    private float dregme;
+    private float ph;
+    private float smelis;
+    private float varis;
+    private float cinkas;
+    
+    
+    
+    public ZemesDetektorius() {
+        
+        
+        
+    }
+
+  
+
+    public float getDregme() {
+        dregme = 5 + rnd.nextFloat()*(30-5);
+        return dregme;
+    }
+
+    public float getPh() {
+        ph = 6 + rnd.nextFloat()*(10-6);
+        return ph;
+    }
+
+    public float getSmelis() {
+        smelis = 50 + rnd.nextFloat()*(60-50);
+        return smelis;
+    }
+
+    public float getVaris() {
+        varis = 14 + rnd.nextFloat()*(18-14);
+        return varis;
+    }
+
+    public float getCinkas() {
+        cinkas = 35 + rnd.nextFloat()*(45-35);
+        return cinkas;
+    }
+
+    
 }
